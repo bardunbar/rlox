3313be64e582b8f2642acd50d3ab4242b8ec822e
bardunbar <ian@dunbargames.com>
2024-05-01 22:49:42 +0100

    Just a little bit more work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
-        1 => println!("Run Prompt"),
-        2 => println!("Run Script: {}", args[1]),
-        _ => println!("Usage: rlox [script]")
+        1 => run_prompt(),
+        2 => run_file(&args[1]),
+        _ => {
+            println!("Usage: rlox [script]");
+            std::process::exit(64)
+        }
     }
+}
+
+fn run_prompt() {
+
+}
+
+fn run_file(path: &str) {
+    let result = fs::read_to_string(path);
+
+}
+
+fn run(source: &str) {
+
 }
